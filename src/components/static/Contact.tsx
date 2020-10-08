5b98e6c50099bb4922e4c96f4ee0beefab9a5b9e
Paul Ly <hwanderlustdev@gmail.com>
2020-10-08 12:16:31 -0400

    basic markup

--- src/components/static/Contact.tsx
+++ src/components/static/Contact.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+import { Disclosure, Logo, MenuBtn } from "../common";
+
+const Title = styled.h1`
+  font-family: 'Playfair Display', serif;
+  font-size: calc(36px + (72 - 36) * ((100vw - 300px) / (1440 - 300)));
+  font-weight: 400;
+`;
 
 function Contact() {
   return (
-    <h1>Contact</h1>
+    <>
+
+      <Title>Contact</Title>
+
+      <Logo />
+      <MenuBtn />
+
+      <h2>Email</h2>
+      <p>Any questions or concerns can be directed to <span>hwanderlustdev@gmail.com</span>.</p>
+
+      <h2>GitHub</h2>
+      <p>Any requests or issues can be directed to <span>github.com/hwanderlust</span>.</p>
+
+      <Disclosure />
+    </>
   );
 }
 
