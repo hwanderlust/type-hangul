81f0c04665916aa23e9579b44b329c75ad380d91
Paul Ly <hwanderlustdev@gmail.com>
2020-10-21 19:13:07 -0400

    remove unused constant

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -29,38 +29,9 @@ const Sizes = {
   },
 }
 
-const ALL_KEYS = [
-  { eng: "q", shift: "ㅃ", kor: "ㅂ" },
-  { eng: "w", shift: "ㅉ", kor: "ㅈ" },
-  { eng: "e", shift: "ㄸ", kor: "ㄷ" },
-  { eng: "r", shift: "ㄲ", kor: "ㄱ" },
-  { eng: "t", shift: "ㅆ", kor: "ㅅ" },
-  { eng: "y", shift: "", kor: "ㅛ" },
-  { eng: "u", shift: "", kor: "ㅕ" },
-  { eng: "i", shift: "", kor: "ㅑ" },
-  { eng: "o", shift: "ㅒ", kor: "ㅐ" },
-  { eng: "p", shift: "ㅖ", kor: "ㅔ" },
-  { eng: "a", shift: "", kor: "ㅁ" },
-  { eng: "s", shift: "", kor: "ㄴ" },
-  { eng: "d", shift: "", kor: "ㅇ" },
-  { eng: "f", shift: "", kor: "ㄹ" },
-  { eng: "g", shift: "", kor: "ㅎ" },
-  { eng: "h", shift: "", kor: "ㅗ" },
-  { eng: "j", shift: "", kor: "ㅓ" },
-  { eng: "k", shift: "", kor: "ㅏ" },
-  { eng: "l", shift: "", kor: "ㅣ" },
-  { eng: "z", shift: "", kor: "ㅋ" },
-  { eng: "x", shift: "", kor: "ㅌ" },
-  { eng: "c", shift: "", kor: "ㅊ" },
-  { eng: "v", shift: "", kor: "ㅍ" },
-  { eng: "b", shift: "", kor: "ㅠ" },
-  { eng: "n", shift: "", kor: "ㅜ" },
-  { eng: "m", shift: "", kor: "ㅡ" },
-];
-
 const fadeIn = keyframes`
   from { opacity: 0; }
   to { opacity: 1; }
 `;
 
-export { ALL_KEYS, Fonts, Sizes, fadeIn };
+export { Fonts, Sizes, fadeIn };
